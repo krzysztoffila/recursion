755486fbfdf38575a0402f2ded8b1133bfbcc439
krzysztoffila <umowy.fila@gmail.com>
2024-01-11 21:35:48 +0100

    Task 11 - Collatz

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('LEEL')
+// https://www.youtube.com/watch?v=5uKtTezJpxk
+// https://www.youtube.com/watch?v=LteNqj4DFD8
+// 11 
+function collatz(n) {
+    if (n === 1) {
+        return 0
+    }
+    return n % 2 === 0 ? collatz(n / 2) + 1 : collatz(3 * n + 1) + 1
+}
+console.log(`Liczba kroków ${collatz(21)}`)
